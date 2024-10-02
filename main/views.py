8e60e67c181fa5c3da139360d77037820fdf46ed
narawaa <nashwa.ghania@ui.ac.id>
2024-10-02 13:57:34 +0700

    feat: Tutorial 5

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils.html import strip_tags
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
@@ -10,22 +11,21 @@ from django.http import HttpResponse
 from django.core import serializers
 from django.shortcuts import render, redirect
 
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
-    mood_entries = MoodEntry.objects.filter(user=request.user)
-
     context = {
         'name': request.user.username,
         'class': 'PBP F',
         'npm': '2306241770',
-        'mood_entries': mood_entries,
         'last_login': request.COOKIES['last_login'],
     }
-
     return render(request, "main.html", context)
 
 def create_mood_entry(request):
@@ -64,11 +64,11 @@ def delete_mood(request, id):
     return HttpResponseRedirect(reverse('main:show_main'))
 
 def show_xml(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = MoodEntry.objects.all()
+    data = MoodEntry.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -101,6 +101,8 @@ def login_user(request):
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
 
     else:
        form = AuthenticationForm(request)
@@ -112,3 +114,20 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+@csrf_exempt
+@require_POST
+def add_mood_entry_ajax(request):
+    mood = strip_tags(request.POST.get("mood")) # strip HTML tags!
+    feelings = strip_tags(request.POST.get("feelings")) # strip HTML tags!
+    mood_intensity = request.POST.get("mood_intensity")
+    user = request.user
+
+    new_mood = MoodEntry(
+        mood=mood, feelings=feelings,
+        mood_intensity=mood_intensity,
+        user=user
+    )
+    new_mood.save()
+
+    return HttpResponse(b"CREATED", status=201)
